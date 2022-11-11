95c61be8d3701778a1a35ddb11a717d879091b70
tplanes <tho.planes@gmail.com>
2022-11-11 17:09:45 +0100

    normed fraw_line

--- project/source/draw_line.c
+++ project/source/draw_line.c
@@ -6,7 +6,7 @@
 /*   By: tplanes <tplanes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 12:35:19 by tplanes           #+#    #+#             */
-/*   Updated: 2022/11/11 13:57:13 by tplanes          ###   ########.fr       */
+/*   Updated: 2022/11/11 17:06:23 by tplanes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	_draw_pix_buff(t_pt2d *t_p0, t_pt2d *t_p1, t_draw *t_d, t_buffer *t_buff);
 void	draw_line_buff(t_pt2d *t_p0, t_pt2d *t_p1, t_buffer *t_buff)
 //nb: could add in box check for p1 and p0
 {
-    t_draw	t_d;
+	t_draw	t_d;
 	t_pt2d	t_p0_cpy;
 
 	t_p0_cpy.x = t_p0 -> x;
@@ -26,17 +26,17 @@ void	draw_line_buff(t_pt2d *t_p0, t_pt2d *t_p1, t_buffer *t_buff)
 	t_d.dx = abs(t_p1 -> x - t_p0 -> x);
 	if (t_p0 -> x < t_p1 -> x)
 		t_d.sx = 1;
-	else 
+	else
 		t_d.sx = -1;
 	t_d.dy = -abs(t_p1 -> y - t_p0 -> y);
-    if (t_p0 -> y < t_p1 -> y)
+	if (t_p0 -> y < t_p1 -> y)
 		t_d.sy = 1;
-	else 
+	else
 		t_d.sy = -1;
-    t_d.error = t_d.dx + t_d.dy; 
-    while (1)
-    {
-        if (_draw_pix_buff(&t_p0_cpy, t_p1, &t_d, t_buff))
+	t_d.error = t_d.dx + t_d.dy;
+	while (1)
+	{
+		if (_draw_pix_buff(&t_p0_cpy, t_p1, &t_d, t_buff))
 			break ;
 	}
 	return ;
@@ -52,15 +52,15 @@ int	_draw_pix_buff(t_pt2d *t_p0, t_pt2d *t_p1, t_draw *t_d, t_buffer *t_buff)
 		return (1);
 	e2 = 2 * t_d -> error;
 	if (e2 >= t_d -> dy)
-	{   
+	{
 		if (t_p0 -> x == t_p1 -> x)
 			return (1);
 		t_d -> error += t_d -> dy;
 		t_p0 -> x += t_d -> sx;
 	}
 	if (e2 <= t_d -> dx)
-	{    
-		if (t_p0 -> y == t_p1 -> y) 
+	{
+		if (t_p0 -> y == t_p1 -> y)
 			return (1);
 		t_d -> error += t_d -> dx;
 		t_p0 -> y += t_d -> sy;
--- project/source/draw_line.h
+++ project/source/draw_line.h
@@ -6,30 +6,30 @@
 /*   By: tplanes <tplanes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 12:38:30 by tplanes           #+#    #+#             */
-/*   Updated: 2022/11/11 13:57:35 by tplanes          ###   ########.fr       */
+/*   Updated: 2022/11/11 17:09:22 by tplanes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdlib.h>
-#include <limits.h>
 
 #ifndef DRAW_LINE_H
 # define DRAW_LINE_H
 
-typedef struct	s_pt2d
+# include <stdlib.h>
+# include <limits.h>
+
+typedef struct s_pt2d
 {
 	int	x;
 	int	y;
 }				t_pt2d;
 
-typedef struct	s_buffer
+typedef struct s_buffer
 {
 	int	nx;
 	int	ny;
 	int	*buff;
 }				t_buffer;
 
-typedef struct	s_draw
+typedef struct s_draw
 {
 	int	dx;
 	int	dy;
