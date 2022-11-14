6721f00eb2fdb43c81ca0f0c7d0da87e3c09c619
tplanes <tho.planes@gmail.com>
2022-11-14 10:29:19 +0100

    parsing

--- project/source/parsing.c
+++ project/source/parsing.c
@@ -6,7 +6,7 @@
 /*   By: tplanes <tplanes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 14:12:57 by tplanes           #+#    #+#             */
-/*   Updated: 2022/11/13 15:46:51 by tplanes          ###   ########.fr       */
+/*   Updated: 2022/11/14 10:28:01 by tplanes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ t_imat	get_input(char *f_name)
 		perror(NULL);
 		exit();
 	}
-	data_in = _parse_file(fd);
+	data_in = _parse_file(fd); //protect?
 	if (close(fd))
 	{
 		perror(NULL);
@@ -35,7 +35,7 @@ t_imat	get_input(char *f_name)
 t_imat	_parse_file(int fd)
 {
 	t_imat	data_in;
-	t_imat	row;
+	int		*row;
 	t_list	*row_list;
 
 	line = get_next_line(fd);
@@ -44,23 +44,27 @@ t_imat	_parse_file(int fd)
 		ft_putstr_fd("Empty file...\n");
 		exit();
 	}
-	row = _parse_line(line);
-	if (row.n < 2)
+	nn = _parse_line(line, &row);
+	free(line);
+	if (row.n < 2) //possibly remove at the end
 	{
 		ft_putstr_fd("Error: at least two columns required!\n");
+		
 		exit();
 	}
-	row_list = NULL;
-	ft_lst_addback(&row_list, ft_lst_new(row.imat));
 	data_in.n = row.n;
+	row_list = ft_lst_new(row.imat); //shld protect lst_new...
+	//ft_lst_addback(&row_list, ft_lst_new(row.imat)); //shld protect lst_new...
 	data_in.m = 1;
 	while (get_next_line(fd))
 	{
+		
 		row = _parse_line(line);
 		if (row.n != data_in.n)
 		{	
 			ft_putstr_fd("Invalid format file!\n");
 			ft_lstclear(&row_list, &free);
+			
 			exit();
 		}
 		ft_lst_addback(&row_list, ft_lst_new(row.imat));
@@ -94,28 +98,29 @@ int	*_list2mat(t_list **row_list, int m, int n)
 	return (data_mat);
 }
 
-t_imat _parse_line(char **line)
+int	_parse_line(char **line, int **row)
 {
-	t_imat	row;
 	char	**word_arr;
 	int		*row_mat;
 	int		n;
 
 	word_arr = ft_split(line);
+	free(line);
+	if (word_arr == NULL)
+		return (-1);
 	n = 0;
 	while (word_arr[n] != NULL)
 		n++;
-	row_mat = (int *)malloc(sizeof(int) * n);
+	*row = (int *)malloc(sizeof(int) * n);
+	if (*row == NULL)
+		return (-1);
 	n = 0;
 	while (word_arr[n] != NULL)
 	{	
-		row_mat[n] = ft_atoi(word_arr[n]);
+		(*row)[n] = ft_atoi(word_arr[n]);
 		free(word_arr[n]);
 		n++;
 	}
 	free(word_arr);
-	row.m = 1;
-	row.n = n;
-	row.imat = row_mat;
-	return (row);
+	return (n);
 }
