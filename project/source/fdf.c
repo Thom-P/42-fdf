db79d670e17bc374aac11f897e2210951c3c8c00
tplanes <tho.planes@gmail.com>
2022-11-18 18:10:14 +0100

    normed fdf.c

--- project/source/fdf.c
+++ project/source/fdf.c
@@ -6,7 +6,7 @@
 /*   By: tplanes <tplanes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 13:44:28 by tplanes           #+#    #+#             */
-/*   Updated: 2022/11/18 18:03:58 by tplanes          ###   ########.fr       */
+/*   Updated: 2022/11/18 18:09:17 by tplanes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,35 +24,37 @@ int	main(int ac, char **av)
 {
 	t_meta	meta;
 
-	_verify_arguments(ac, av);		
+	_verify_arguments(ac, av);
 	meta.data_in = get_input(av[1]);
 	_create_win(&meta.xp, WIN_NY, WIN_NX, "***Fil de Fer***");
-	_create_image(&meta.xp, &meta.im);	
+	_create_image(&meta.xp, &meta.im);
 	create_init_fmat(&meta.data_in, &meta.init_fmat, &meta.im);
 	meta.view.theta_z = THETA_Z_ISO;
 	meta.view.theta_x = THETA_X_ISO;
 	process_and_render(&meta);
-    mlx_hook(meta.xp.win, KEY_DOWN, 0, &key_hook, &meta);
-	mlx_hook(meta.xp.win, DESTROY_WIN, 0, &destroy_hook, &meta.xp);		
+	mlx_hook(meta.xp.win, KEY_DOWN, 0, &key_hook, &meta);
+	mlx_hook(meta.xp.win, DESTROY_WIN, 0, &destroy_hook, &meta.xp);
 	mlx_loop(meta.xp.mlx);
 	return (0);
 }
 
-// dup init mat to always start back from init state and not prop error
+//dup init mat to always start back from init state and not prop error
 //nb: put data_in sizes in indep struct to avoid carrying data_in around?
 //apply zoom (eg +/- ?)  do both directly in proj op?
-//apply shift (arrows /maj for small)//need to free init ma//data in passed only for dimensions (only mat freed)t at closure?
+//apply shift (arrows /maj for small)//need to free init ma
+//data in passed only for dimensions (only mat freed)t at closure?
 //replace destr and creat by a ima_clean fct to reset pix?
 void	process_and_render(t_meta *meta)
 {
-	t_fmat fmat;
+	t_fmat	fmat;
 
-	fmat = fmat_dup(&meta -> init_fmat); 
+	fmat = fmat_dup(&meta -> init_fmat);
 	rotate_fmat(&fmat, meta -> view.theta_z, meta -> view.theta_x);
-	draw_grid_image(&fmat, &meta -> im, &meta -> data_in); 
+	draw_grid_image(&fmat, &meta -> im, &meta -> data_in);
 	free(fmat.fmat);
-	mlx_put_image_to_window(meta -> xp.mlx, meta -> xp.win, meta -> im.id, meta -> im.pos_x, meta -> im.pos_y);
-	mlx_destroy_image(meta -> xp.mlx, meta -> im.id); 
+	mlx_put_image_to_window(meta -> xp.mlx, meta -> xp.win,
+		meta -> im.id, meta -> im.pos_x, meta -> im.pos_y);
+	mlx_destroy_image(meta -> xp.mlx, meta -> im.id);
 	_create_image(&meta -> xp, &meta -> im);
 	return ;
 }
@@ -94,7 +96,8 @@ static void	_create_image(t_xptr *xp, t_image *im)
 	im -> pos_x = round(0.5 * (WIN_NX - im -> nx));
 	im -> pos_y = round(0.5 * (WIN_NY - im -> ny));
 	im -> id = mlx_new_image(xp -> mlx, im -> nx, im -> ny);
-	im -> addr = mlx_get_data_addr(im -> id, &im -> bpp, &im -> line_size, &im -> endian);
+	im -> addr = mlx_get_data_addr(im -> id, &im -> bpp,
+			&im -> line_size, &im -> endian);
 	draw_box_around_image(im);
 	return ;
 }
