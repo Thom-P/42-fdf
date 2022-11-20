fac7547581b315b0cf701a2f8157d545c76f5af4
tplanes <tho.planes@gmail.com>
2022-11-20 22:16:57 +0100

    reverted to gcc -O2 because couldnt install valgrind on macos 12

--- project/source/comp_fdf.sh
+++ project/source/comp_fdf.sh
@@ -3,3 +3,6 @@
 #gcc -Wall -Wextra -Werror -Wconversion -O2 -I../include/ -L../lib/ -lmlx -lft -framework OpenGL -framework AppKit *.c -o fdf
 
 gcc -Wall -Wextra -Werror -O2 -I../include/ -L../lib/ -lmlx -lft -framework OpenGL -framework AppKit *.c -o fdf
+
+#for valgrind
+#gcc -Wall -Wextra -Werror -O0 -I../include/ -L../lib/ -lmlx -lft -framework OpenGL -framework AppKit *.c -g -o fdf
